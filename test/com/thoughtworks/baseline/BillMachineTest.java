260abbf954007cf7473e8b491ed158bd5020ce07
srikantk <kaanth.katta@gmail.com>
2015-08-28 18:30:58 +0530

    spec for other goods calculating cost along with sales tax

--- test/com/thoughtworks/baseline/BillMachineTest.java
+++ test/com/thoughtworks/baseline/BillMachineTest.java
@@ -7,16 +7,23 @@ import static org.junit.Assert.*;
 public class BillMachineTest {
 
     @Test
-    public void costsZeroWhenNoItemsAddedToShoppingBasket() {
+    public void givesZeroPriceWhenNoItemsAddedToShoppingBasket() {
         BillMachine machine = new BillMachine("", 0.0);
 
         assertEquals(0, machine.calculateTotalCost(), 0.2d);
     }
 
     @Test
-    public void costsToBookPriceWhenSingleBookAdded() {
+    public void givesItsPriceWhenSingleBookAddedToShoppingBasket() {
         BillMachine machine = new BillMachine("Book", 12.0);
 
         assertEquals(12.0, machine.calculateTotalCost(), 0.2d);
+    }
+
+    @Test
+    public void giveItsPriceAlongSalesTaxWhenOtherGoodsAddedToShoppingBasket() {
+        BillMachine machine = new BillMachine("MusicCD", 12.0);
+
+        assertEquals(13.2,machine.calculateTotalCost(),0.2d);
     }
 }
