2edccfd2d4df88d237ca749417106ec7ce410a3f
srikantk <kaanth.katta@gmail.com>
2015-08-28 17:44:52 +0530

    cost equals to zero when no items added to shopping basket

---
+++ README.md
@@ -0,0 +1,8 @@
+#PROBLEM DISCRIPTION#
+    Calculating sales tax on goods
+#DEV ENVIRONMENT#
+    Any os
+    JDK
+    ANT
+#BUILD CODEBASE#
+    ant build
---
+++ src/com/thoughtworks/baseline/ShoppingBasket.java
@@ -0,0 +1,11 @@
+package com.thoughtworks.baseline;
+
+public class ShoppingBasket {
+
+    public ShoppingBasket(String item, double pirice) {
+    }
+
+    public double calculateTotalCost() {
+        return 0;
+    }
+}
